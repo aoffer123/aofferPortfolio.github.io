b2107fee1408f978ecb8e65d52fb3a71cf7d528a
aoffer123 <aoffer@oakland.edu>
2024-07-12 12:21:35 -0400

    styling changes

--- js/skills.js
+++ js/skills.js
@@ -32,3 +32,14 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('downloadResumeBtn').addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = 'assets/AllisonOfferResume.pdf';  // Update with the correct path to your resume file
+        link.download = 'AllisonOffer_Resume.pdf';  // Update with the desired file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
