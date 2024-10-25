1d07bb41bb91f6876440d87ee67e28d15c537a31
aoffer123 <aoffer@oakland.edu>
2024-10-25 16:10:11 -0400

    fixing resume

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('downloadResumeBtn').addEventListener('click', function () {
         const link = document.createElement('a');
-        link.href = '../assets/AllisonOffer-Resume.pdf';  // Update with the correct path to your resume file
+        link.href = '../assets/Allison-Offer-RevResume.pdf';  // Update with the correct path to your resume file
         link.download = 'AllisonOfferResume.pdf';  // Update with the desired file name
         document.body.appendChild(link);
         link.click();
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function downloadResume() {
   const link = document.createElement('a');
-  link.href = '../assets/AllisonOffer-Resume.pdf';
-  link.download = 'AllisonOffer-Resume.pdf';
+  link.href = '../assets/Allison-Offer-RevResume.pdf';
+  link.download = 'Allison-Offer-RevResume.pdf';
+  document.body.appendChild(link); // Required for Firefox
   link.click();
+  document.body.removeChild(link); // Clean up
 }
